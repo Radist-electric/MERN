05c2cc1c347478b8cf9118207d627710266c8a7c
Radist <radist-electric@yandex.ru>
2020-11-07 16:12:08 +0500

    I installed React, created basic structure of client application and created auth/login form

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export const AuthPage = () => {
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Сократи Ссылку</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Авторизация</span>
+            <div>
+              <div className="input-field">
+                <input
+                  placeholder="Введите email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                />
+                <label htmlFor="email">First Name</label>
+              </div>
+              <div className="input-field">
+                <input
+                  placeholder="Введите пароль"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                />
+                <label htmlFor="password">First Name</label>
+              </div>
+              </div>
+            </div>
+            <div className="card-action">
+              <button className="btn yellow darken-4 mr-10">Войти</button>
+              <button className="btn grey lighten-1 black-text">Регистрация</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
