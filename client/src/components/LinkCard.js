808cd67a9d59c9ebe5e5da0142b4c5fdb4d5658f
Radist <radist-electric@yandex.ru>
2020-11-10 22:29:14 +0500

    I created a page for a table of links

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -5,7 +5,7 @@ export const LinkCard = ({ link }) => {
     <>
       <h2>Ссылка</h2>
       <p>Ваша ссылка: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
-      <p>Откуда: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
+      <p>Откуда: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
       <p>Количество кликов по ссылке: <strong>{link.click}</strong></p>
       <p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
     </>
