38abc007be8627797d306f4d5ca4a4eeb5b81600
Radist <radist-electric@yandex.ru>
2020-11-10 12:10:17 +0500

    I added navbar and backend code for links

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model, ObTypes, Types } = require('mongoose')
+const { Schema, model, Types } = require('mongoose')
 const schema = new Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
